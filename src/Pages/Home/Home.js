9a45032e0e2350e6ce91d93189bb325e3e196512
Seb <sebcyde@hotmail.com>
2022-02-04 22:26:44 +0000

    added in scrolling text container on the homepage

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,7 +4,17 @@ import { Navbar, Icon, NavItem } from 'react-materialize';
 import './Home.css';
 
 function Home() {
-	return <div className="HomeContainer">Home</div>;
+	return (
+		<div className="HomeContainer">
+			<div id="scroll-container">
+				<div id="scroll-text">
+					For a limited time only, you can get 10% off your ENTIRE order with
+					code MINTED10. Offer valid until 10th February 2022.
+				</div>
+			</div>
+			Home
+		</div>
+	);
 }
 
 export default Home;
