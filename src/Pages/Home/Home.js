038a054671e12ebe126307e6977d068d58be9401
Seb <sebcyde@hotmail.com>
2022-02-04 21:33:43 +0000

    removed secondary materialize nav, will add as simple links on homepage due to too many bugs

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,31 +4,7 @@ import { Navbar, Icon, NavItem } from 'react-materialize';
 import './Home.css';
 
 function Home() {
-	return (
-		<div className="HomeContainer">
-			<Navbar
-				className="BottomLevelNav"
-				alignLinks="right"
-				id="mobile-nav"
-				options={{
-					draggable: true,
-					edge: 'left',
-					inDuration: 250,
-					onCloseEnd: null,
-					onCloseStart: null,
-					onOpenEnd: null,
-					onOpenStart: null,
-					outDuration: 200,
-					preventScrolling: true,
-				}}
-			>
-				<NavItem href="">Products</NavItem>
-				<NavItem href="">Cart</NavItem>
-				<NavItem href="">Sign In</NavItem>
-			</Navbar>
-			Home
-		</div>
-	);
+	return <div className="HomeContainer">Home</div>;
 }
 
 export default Home;
